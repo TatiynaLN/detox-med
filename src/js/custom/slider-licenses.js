9186e3c564511fa030487404abc04958cfed129a
TatiynaLN <tania.cis2011@yandex.ru>
2021-05-10 16:59:07 +0300

    fix

--- src/js/custom/slider-licenses.js
+++ src/js/custom/slider-licenses.js
@@ -1,35 +1,45 @@
-// ;(function () {
+; (function () {
+    new Swiper('.slider-licenses', {
+        navigation: {
+            nextEl: '.slider-licenses-next',
+            prevEl: '.slider-licenses-prev'
+        },
+        // управление клавиатурой
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+            pageUpDown: true,
+        },
+        watchOverflow: true,
+        autoHeight: false,
+        slidePerGroup: 1,
+        initialSlide: 0,
+        speed: 400,
+        loop: false, // бесконечный слайд
+        breakpoints: {
+            // when window width is <= 320px
+            320: {
+                autoHeight: true,
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window width is <= 480px
+            520: {
 
-//   if(document.querySelector('.swiper-licenses')) {
-//     const swiperLicenses = new Swiper('.swiper-licenses', {
-//       spaceBetween: 30,
-//       slidesPerView: 4,
-//       loopedSlides: 1,
-//       // grabCursor: true,
-//       // loopAdditionalSlides: 4,
-//       // loop: true,
-//       navigation: {
-//         nextEl: '.swiper-button-next--licenses',
-//         prevEl: '.swiper-button-prev--licenses',
-//       },
-//       breakpoints: {
-//         320: {
-//           slidesPerView: 1,
-//           loopedSlides: 1,
-//           spaceBetween: 0
-//         },
-//         768: {
-//           slidesPerView: 2,
-//           loopedSlides: 1,
-//           spaceBetween: 20
-//         },
-//         1320: {
-//           slidesPerView: 4,
-//           loopedSlides: 1,
-//           spaceBetween: 20
-//         },
-//       }
-//     });
-//   }
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is <= 640px
+            1024: {
+                initialSlide: 1,
+                centeredSlides: false,
+                slidesPerView: 3,
+                spaceBetween: 38
 
-// })();
+            }
+        }
+    });
+
+
+})();
+
