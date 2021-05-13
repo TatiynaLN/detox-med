10619e72cf2866beb75480fa3d7102a3e22c6c38
TatiynaLN <tania.cis2011@yandex.ru>
2021-05-13 15:31:53 +0300

    fix

--- src/js/custom/link.js
+++ src/js/custom/link.js
@@ -1,19 +1,19 @@
-// ; (function () {
-//     // ДОБАВЛЯЕМ КЛАСС АКТИВНОЙ ССЫЛКЕ БЛОК .belt__tags
+; (function () {
+    // ДОБАВЛЯЕМ КЛАСС АКТИВНОЙ ССЫЛКЕ БЛОК .belt__tags
 
-//     const items = document.querySelectorAll(".belt__tags-link");
-//     for (let i = 0; i < items.length; i++) {
-//         const item = items[i];
-//         item.addEventListener("click", changeActiveClass);
-//     }
+    const items = document.querySelectorAll(".belt__tags-link");
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        item.addEventListener("click", changeActiveClass);
+    }
 
-//     function changeActiveClass(e) {
-//         for (let i = 0; i < items.length; i++) {
-//             const item = items[i];
-//             item.classList.remove('link-active');
-//         }
-//         e.target.classList.add('link-active');
-//     }
+    function changeActiveClass(e) {
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            item.classList.remove('link-active');
+        }
+        e.target.classList.add('link-active');
+    }
 
 
-// })();
+})();
