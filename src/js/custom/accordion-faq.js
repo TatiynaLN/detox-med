a71329244d5d2cf9467ffdf083068f7276654b37
TatiynaLN <tania.cis2011@yandex.ru>
2021-06-01 10:20:40 +0300

    fix

--- src/js/custom/accordion-faq.js
+++ src/js/custom/accordion-faq.js
@@ -1,20 +1,24 @@
-// ; (function () {
 
-//     if (document.querySelector('.questions')) {
+
+// ; (function () {
+//     if (document.querySelector('.questions-open')) {
 //         let accordItemsTitle = document.querySelectorAll('.accordion-first .accordion__item h3');
 //         let accordItems = document.querySelectorAll('.accordion-first .accordion__item');
 //         let accordItemsContent = document.querySelectorAll('.accordion-first .accordion__item p');
 
 //         function openCard() {
-
 //             let activeInitial = accordItemsContent[0].getBoundingClientRect().height;
-//             accordItems[0].style.minHeight = `${activeInitial + 90}px`;
+//             accordItems[0].style.minHeight = `${activeInitial + 160}px`;
 //             accordItems[0].classList.add('accordion__item--active');
-//             // accordItems[0].style.minHeight = `${200}px`;
 
 //             accordItemsTitle.forEach((item, i) => {
+//                 let titleHeightInitial = accordItemsTitle[i].getBoundingClientRect().height;
+//                 accordItems[0].style.height = `${titleHeightInitial}px`;
+//                 accordItems[0].classList.add('accordion__item--active');
 //                 item.addEventListener('click', () => {
+//                     let titleHeightInitial = accordItemsTitle[i].getBoundingClientRect().height;
 //                     let contentHeight = accordItemsContent[i].getBoundingClientRect().height;
+//                     console.log(titleHeightInitial);
 //                     if (accordItems[i].classList.contains('accordion__item--active')) {
 //                         setTimeout(() => {
 //                             accordItems[i].classList.remove('accordion__item--active');
@@ -25,7 +29,7 @@
 //                         }, 100)
 //                     } else {
 //                         setTimeout(() => {
-//                             let newHeight = contentHeight + 0;
+//                             let newHeight = contentHeight + 64;
 //                             accordItems[i].style.minHeight = `${newHeight}px`;
 //                             accordItems[i].classList.add('accordion__item--active');
 //                         }, 200)
@@ -38,49 +42,121 @@
 //         }
 //         openCard();
 //     }
-
 // })();
 
 ; (function () {
-    if (document.querySelector('.questions-open')) {
-        let accordItemsTitle = document.querySelectorAll('.accordion-first .accordion__item h3');
-        let accordItems = document.querySelectorAll('.accordion-first .accordion__item');
-        let accordItemsContent = document.querySelectorAll('.accordion-first .accordion__item p');
 
+    if (document.querySelector('.open')) {
+        let accordItemsTitle = document.querySelectorAll('.open .accordion__item h3');
+        let accordItemsContent = document.querySelectorAll('.open .accordion__item p');
+        console.log(accordItemsTitle);
         function openCard() {
-            let activeInitial = accordItemsContent[0].getBoundingClientRect().height;
-            accordItems[0].style.minHeight = `${activeInitial + 160}px`;
-            accordItems[0].classList.add('accordion__item--active');
+            accordItemsTitle.forEach(item => {
+                accordItemsContent[0].style.height = `${accordItemsContent[0].scrollHeight}px`;
+                accordItemsContent[0].classList.add('accordion__content--active');
+                accordItemsTitle[0].classList.add('accordion__item--active');
 
-            accordItemsTitle.forEach((item, i) => {
-                let titleHeightInitial = accordItemsTitle[i].getBoundingClientRect().height;
-                accordItems[0].style.height = `${titleHeightInitial}px`;
-                accordItems[0].classList.add('accordion__item--active');
                 item.addEventListener('click', () => {
-                    let titleHeightInitial = accordItemsTitle[i].getBoundingClientRect().height;
-                    let contentHeight = accordItemsContent[i].getBoundingClientRect().height;
-                    console.log(titleHeightInitial);
-                    if (accordItems[i].classList.contains('accordion__item--active')) {
-                        setTimeout(() => {
-                            accordItems[i].classList.remove('accordion__item--active');
-                            accordItems[i].style.minHeight = `${64}px`;
-                        }, 300)
-                        setTimeout(() => {
-                            accordItemsContent[i].classList.remove('accordion__content--active');
-                        }, 100)
+                    if (item.nextElementSibling.classList.contains('accordion__content--active')) {
+                        hideText(item.nextElementSibling, item);
                     } else {
-                        setTimeout(() => {
-                            let newHeight = contentHeight + 64;
-                            accordItems[i].style.minHeight = `${newHeight}px`;
-                            accordItems[i].classList.add('accordion__item--active');
-                        }, 200)
-                        setTimeout(() => {
-                            accordItemsContent[i].classList.add('accordion__content--active');
-                        }, 800)
+                        showText(item.nextElementSibling, item);
                     }
                 });
             });
         }
         openCard();
+
+        function showText(textEl, item) {
+            textEl.style.height = `${textEl.scrollHeight}px`;
+            textEl.classList.add('accordion__content--active');
+            item.classList.add('accordion__item--active');
+        }
+
+        function hideText(textEl, item) {
+            textEl.style.height = `${0}px`;
+            textEl.classList.remove('accordion__content--active');
+            item.classList.remove('accordion__item--active');
+        }
+
+        window.addEventListener('resize', () => {
+            openCard();
+        });
+    }
+    //
+    if (document.querySelector('.close')) {
+        let accordItemsTitle = document.querySelectorAll('.close .accordion__item h3');
+        let accordItemsContent = document.querySelectorAll('.close .accordion__item p');
+        console.log(accordItemsTitle);
+        function openCard() {
+            accordItemsTitle.forEach(item => {
+                // accordItemsContent[0].style.height = `${accordItemsContent[0].scrollHeight}px`;
+                // accordItemsContent[0].classList.add('accordion__content--active');
+                // accordItemsTitle[0].classList.add('accordion__item--active');
+
+                item.addEventListener('click', () => {
+                    if (item.nextElementSibling.classList.contains('accordion__content--active')) {
+                        hideText(item.nextElementSibling, item);
+                    } else {
+                        showText(item.nextElementSibling, item);
+                    }
+                });
+            });
+        }
+        openCard();
+
+        function showText(textEl, item) {
+            textEl.style.height = `${textEl.scrollHeight}px`;
+            textEl.classList.add('accordion__content--active');
+            item.classList.add('accordion__item--active');
+        }
+
+        function hideText(textEl, item) {
+            textEl.style.height = `${0}px`;
+            textEl.classList.remove('accordion__content--active');
+            item.classList.remove('accordion__item--active');
+        }
+
+        window.addEventListener('resize', () => {
+            openCard();
+        });
+    }
+    //
+    if (document.querySelector('.accordion-second')) {
+        let accordItemsTitle = document.querySelectorAll('.accordion-second .accordion__item h3');
+        let accordItemsContent = document.querySelectorAll('.accordion-second .accordion__item p');
+        console.log(accordItemsTitle);
+        function openCard() {
+            accordItemsTitle.forEach(item => {
+                // accordItemsContent[0].style.height = `${accordItemsContent[0].scrollHeight}px`;
+                // accordItemsContent[0].classList.add('accordion__content--active');
+                // accordItemsTitle[0].classList.add('accordion__item--active');
+
+                item.addEventListener('click', () => {
+                    if (item.nextElementSibling.classList.contains('accordion__content--active')) {
+                        hideText(item.nextElementSibling, item);
+                    } else {
+                        showText(item.nextElementSibling, item);
+                    }
+                });
+            });
+        }
+        openCard();
+
+        function showText(textEl, item) {
+            textEl.style.height = `${textEl.scrollHeight}px`;
+            textEl.classList.add('accordion__content--active');
+            item.classList.add('accordion__item--active');
+        }
+
+        function hideText(textEl, item) {
+            textEl.style.height = `${0}px`;
+            textEl.classList.remove('accordion__content--active');
+            item.classList.remove('accordion__item--active');
+        }
+
+        window.addEventListener('resize', () => {
+            openCard();
+        });
     }
 })();
--- src/js/custom/min-accordion.js
+++ src/js/custom/min-accordion.js
@@ -1,77 +1,36 @@
-// ; (function () {
 
-//     if (document.querySelector('.cost')) {
-//         let accordItemsTitle = document.querySelectorAll('.accordion-second .accordion__item h3');
-//         let accordItems = document.querySelectorAll('.accordion-second .accordion__item');
-//         let accordItemsContent = document.querySelectorAll('.accordion-second .accordion__item p');
+// ; (function () {
+//     if (document.querySelector('.cost') || document.querySelector('.questions-close')) {
+//         let accordItemsTitle = document.querySelectorAll('.accordion__item h3');
+//         let accordItems = document.querySelectorAll('.accordion__item');
+//         let accordItemsContent = document.querySelectorAll('.accordion__item p');
 
 //         function openCard() {
-
-//             // let activeInitial = accordItemsContent[0].getBoundingClientRect().height;
-//             // accordItems[0].style.minHeight = `${activeInitial + 116}px`;
-//             // accordItems[0].classList.add('accordion__item--active2');
-//             // accordItems[0].style.minHeight = `${200}px`;
-
 //             accordItemsTitle.forEach((item, i) => {
+//                 let titleHeightInitial = accordItemsTitle[i].getBoundingClientRect().height;
+//                 accordItems[i].style.height = `${titleHeightInitial}px`;
 //                 item.addEventListener('click', () => {
+//                     let titleHeightInitial = accordItemsTitle[i].getBoundingClientRect().height;
 //                     let contentHeight = accordItemsContent[i].getBoundingClientRect().height;
+//                     console.log(titleHeightInitial);
 //                     if (accordItems[i].classList.contains('accordion__item--active')) {
+//                         accordItemsContent[i].classList.remove('accordion__content--active');
 //                         setTimeout(() => {
 //                             accordItems[i].classList.remove('accordion__item--active');
-//                             accordItems[i].style.minHeight = `${0}px`;
-//                         }, 300)
-//                         setTimeout(() => {
-//                             accordItemsContent[i].classList.remove('accordion__content--active');
-//                         }, 100)
+//                             accordItems[i].style.height = `${titleHeightInitial}px`;
+//                         }, 200);
 //                     } else {
-//                         setTimeout(() => {
-//                             let newHeight = contentHeight + 116;
-//                             accordItems[i].style.minHeight = `${newHeight}px`;
-//                             accordItems[i].classList.add('accordion__item--active');
-//                         }, 200)
+//                         let titleHeight = accordItemsTitle[i].getBoundingClientRect().height;
+//                         let newHeight = `${contentHeight + titleHeight + 47}`;
+//                         accordItems[i].style.height = `${newHeight}px`;
+//                         accordItems[i].classList.add('accordion__item--active');
 //                         setTimeout(() => {
 //                             accordItemsContent[i].classList.add('accordion__content--active');
-//                         }, 800)
+//                         }, 300);
 //                     }
 //                 });
 //             });
 //         }
 //         openCard();
 //     }
-
 // })();
-; (function () {
-    if (document.querySelector('.cost') || document.querySelector('.questions-close')) {
-        let accordItemsTitle = document.querySelectorAll('.accordion__item h3');
-        let accordItems = document.querySelectorAll('.accordion__item');
-        let accordItemsContent = document.querySelectorAll('.accordion__item p');
-
-        function openCard() {
-            accordItemsTitle.forEach((item, i) => {
-                let titleHeightInitial = accordItemsTitle[i].getBoundingClientRect().height;
-                accordItems[i].style.height = `${titleHeightInitial}px`;
-                item.addEventListener('click', () => {
-                    let titleHeightInitial = accordItemsTitle[i].getBoundingClientRect().height;
-                    let contentHeight = accordItemsContent[i].getBoundingClientRect().height;
-                    console.log(titleHeightInitial);
-                    if (accordItems[i].classList.contains('accordion__item--active')) {
-                        accordItemsContent[i].classList.remove('accordion__content--active');
-                        setTimeout(() => {
-                            accordItems[i].classList.remove('accordion__item--active');
-                            accordItems[i].style.height = `${titleHeightInitial}px`;
-                        }, 200);
-                    } else {
-                        let titleHeight = accordItemsTitle[i].getBoundingClientRect().height;
-                        let newHeight = `${contentHeight + titleHeight + 47}`;
-                        accordItems[i].style.height = `${newHeight}px`;
-                        accordItems[i].classList.add('accordion__item--active');
-                        setTimeout(() => {
-                            accordItemsContent[i].classList.add('accordion__content--active');
-                        }, 300);
-                    }
-                });
-            });
-        }
-        openCard();
-    }
-})();
