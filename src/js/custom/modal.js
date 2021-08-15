62b1045ab1f570871a324d5e4edf920e37e1f308
LLEEVV2020 <lleevv2020@gmail.com>
2021-08-15 22:47:59 +0400

    add

--- src/js/custom/modal.js
+++ src/js/custom/modal.js
@@ -377,5 +377,145 @@
         }
     });
     // modalThanks();
+
+    ///////////////////////////////
+    //  Скрипты для для форм
+    ///////////////////////////////
+
+    // телефон заполнить полностью
+    let phoneText = false;
+    // емайл заполнить полностью
+    let emailText = false; 
+
+    var h_form_btn = document.querySelectorAll('.btn-send');
+
+    h_form_btn.forEach(function (item) {
+        formReadyClick(item);
+    });
+
+    function formReadyClick (btn) { 
+
+        let form = btn.closest("form");
+        
+        form.addEventListener("submit", formSendAsync2, {once: true});
+        
+        async function formSendAsync2(e){
+
+            e.preventDefault(); // запрет на отправку стандартной формы
+            
+            formSendAsync(btn);
+        }
+    }
+
+    async function formSendAsync(btn_child){
+
+        let form = btn_child.closest("form");
+        
+        let error = formValidate(form);
+
+        let formData = new FormData(form);
+
+        //form.preventDefault(); // запрет на отправку стандартной формы
+
+        if(error === 0){
+
+            form.classList.add('_sending');
+            let response = await fetch('/wp-content/themes/genesis/lib/ajax-send.php', {
+            method: 'POST',
+                body: formData
+            });
+            if(response.ok){
+                let result = await response.json();
+
+                form.reset();
+                form.classList.remove('_sending');
+
+                setTimeout(() => {
+                    modalThanks(options[3].modal_call_title, options[3].modal_call_subtitle);
+                }, 1000);
+
+                return true;
+
+            } else {
+                alert("ошибка");
+            }
+
+        } else {
+            if(phoneText){
+                alert('Введите ещё цифры телефона');
+            } else if(emailText)  {
+                alert('Введите емайл полностью');
+            } else {
+                alert('заполните поля');
+            }
+        }
+        return false;
+    }
+
+    // проверка на ошибки
+    function formValidate(form){
+        let error = 0;
+        //let formReq = document.querySelectorAll('._req'); // обязательное поле
+        let formReq = form.getElementsByClassName('_req');
+
+        for (let index = 0; index < formReq.length; index++){
+            const input = formReq[index];
+            formRemoveError(input); // убрать класс проверки
+
+            if(input.classList.contains('_email')){
+                if(emailTest(input)){
+                    formAddError(input);
+                    error++;
+                }
+            }
+            else if(input.classList.contains('input__mask')){
+                if(phoneTest(input)){
+                    formAddError(input);
+                    error++;
+                    phoneText = true;
+                }
+
+            }
+            else if(input.getAttribute("type") === "checkbox" && input.checked === false){
+                formAddError(input);
+                error++;  
+            } else {
+                if(input.value === ''){
+                    formAddError(input);
+                    error++; 
+                }
+            }
+        }
+        return error;
+    }
+    // добавляют родители и элементу класс _error
+    function formAddError(input){
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input){
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    // проверка emai
+    function emailTest(input){
+        emailText = true;
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+
+    // проверка телефона
+    function phoneTest(input){
+        if(input.value.replace(/ +/g, ' ').trim() === "+7" || 
+        input.value === '' || 
+        input.value.replace(/[\+\(\)\s]/g,"").length < 12 
+        ){
+            //let hhh = input.value.replace(/[\+\(\)\s]/g,"").length;
+        return true ;
+        }
+        return false;
+    }
+
+
 })();
 
